0948280f749c6c50905ae5c09ce56905ab064124
mariav28 <cavichiolo.maria@escola.pr.gov.br>
2025-10-24 10:45:16 -0300

    alteração do conteudo das paginas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./componentes/NavBar";
+import Home from "./componentes/Home";
+import Perfil from "./componentes/Perfil";
+import Escolaridade from "./componentes/Escolaridade";
+import Footer from "./componentes/Footer";
+import Cursos from "./componentes/Cursos"
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function app(){
+  return(
+    <div className="font-sans">
+      <NavBar />
+      <Home />
+      <Perfil />
+      <Escolaridade />
+      <Cursos/>
+      <Footer/>
+
     </div>
   );
 }
-
-export default App;

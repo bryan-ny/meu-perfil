88dd901c9f5599b94c034a8dddc17e07cd3c1216
mariav28 <cavichiolo.maria@escola.pr.gov.br>
2025-10-24 10:45:47 -0300

    alterações do conteudo das paginas

---
+++ src/componentes/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function footer(){
+  return(
+    <footer className="Footer">
+      <div className="Footer-bottom">&copy DIREITOS RESERVADOS DE BRYAN ANDRÉ FERREIRA MACHADO</div> 
+    </footer>
+  );
+}
